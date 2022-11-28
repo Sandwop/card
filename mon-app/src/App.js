e2e25f563972582fe782efb176f58adfa16f618e
Sandwop <74904204+Sandwop@users.noreply.github.com>
2022-11-28 13:12:10 +0100

    columns

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -2,6 +2,10 @@
 function App() {
   return (
     <div className="App">
+      <div className='colLeft column'>
+      </div>
+      <div className='colRight column'>
+      </div>
     </div>
   );
 }
