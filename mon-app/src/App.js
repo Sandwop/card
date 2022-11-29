8a2e5b5a83c51c13d0b45a65de68f898c5b8031a
Sandwop <74904204+Sandwop@users.noreply.github.com>
2022-11-29 10:53:47 +0100

    script nombres

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -4,27 +4,38 @@ import {useState} from "react";
 
 function App() {
 
-  const ([])
+  const [number, setNumber] = useState('');
+  const format = number.match(/.{1,4}/g);
+
+  const [name, setName] = useState('');
+
+  const [month, setMonth] = useState('');
+
+  const [year, setYear] = useState('');
+
 
   return (
     <div className="App">
       <div className="firstCard">
-        <p className="cardNumber">123</p>
+        <p className="cardNumber" >{Array.isArray(format) ? format.join(' ') : '0000 0000 0000 0000'}</p>
+        <p className="cardName" >{name.length > 0 ? name : 'Jane Appleseed'}</p>
+        <p className="cardDate" >{month.length > 0 ? month : '00'}/{year.length > 0 ? month : '00'}</p>
+        
       </div>
       <div className='colLeft column'>
       </div>
       <div className='colRight column'>
         <form>
           <label>Cardholder name</label>
-          <input type="text" placeholder="e.g. Jane Appleseed"></input>
+          <input type="text" placeholder="e.g. Jane Appleseed" value={name} onChange={e => setName(e.target.value)}></input>
           <label>Card number</label>
-          <input type="number" placeholder="e.g. 1234 5678 1234 5678"></input>
+          <input type="tel" pattern="\d*" maxlength="16" value={number} placeholder="e.g. 1234 5678 1234 5678" onChange={e => setNumber(e.target.value)}></input>
           <div className="inputs">
             <div className="dates">
               <label>Exp. date (mm/y)</label>
               <div>
-                <input type='number' placeholder="MM"></input>
-                <input type='number' placeholder="YY"></input>
+                <input type='number' placeholder="MM" value={month} onChange={e => setMonth(e.target.value)}></input>
+                <input type='number' placeholder="YY" value={year} onChange={e => setYear(e.target.value)}></input>
               </div>
             </div>
             <div>
